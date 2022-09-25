fbefa49a6194db190e81130b2149a6522a8c5981
Mitali Nikumbh <74778603+mitalinikumbh@users.noreply.github.com>
2022-09-25 20:13:55 +0530

    Update script.js

--- TASK1-WEATHER_APP_USING_API/script.js
+++ TASK1-WEATHER_APP_USING_API/script.js
@@ -4,34 +4,8 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const { url } = require("url");
 
-
-
-
-// const app = express();
-// app.use(bodyParser.urlencoded({extended:true}));
-
 const mainfile = fs.readFileSync(`${__dirname}/index.html`, "utf-8");
 
-// app.get('/', (req,res)=> {
-//   res.sendFile(`${__dirname}/templates/index.html`);
-// })
-
-// app.post('/', (req, res) => {
-//   // console.log(req.body.cityname);
-//   console.log("the request is received");
-//   console.log(req.body.search_val);
-// })
-
-
-// const form = document.querySelector('.search');
-// form.addEventListener("submit", e =>{
-//   e.preventDefault();
-//   const inputVal = input.value;
-// })
-
-// const inputVal = input.value;
-
-//html
 const replaceVal = (tempVal, orgVal) => {
   let temperture = tempVal.replace(
     "{%DESCRIP%}",
@@ -43,19 +17,15 @@ const replaceVal = (tempVal, orgVal) => {
   temperture = temperture.replace("{%WINDSPEED%}", orgVal.wind.speed);
   temperture = temperture.replace("{%LOCATIONVAL%}", orgVal.name);
   temperture = temperture.replace("{%COUNTRY%}", orgVal.sys.country);
-  // temperture = temperture.replace("{%tempstatus%}", orgVal.sys.country);
   temperture = temperture.replace("{%minTempVal%}", orgVal.main.temp_min);
   temperture = temperture.replace("{%maxTempVal%}", orgVal.main.temp_max);
   return temperture;
 };
 
 
-
-
-
 const server = http.createServer((req, res) => {
   const querry = "Pune";
-  const apiKey = "d8bae6905e1cc3eaf2624f5498368048";
+  const apiKey = "d8bae6905e1cc3eaf******498368048";   // Use your OpenWeather API Key
   if (req.url == "/") {
     requests(
       "https://api.openweathermap.org/data/2.5/weather?appid=" +
@@ -66,13 +36,11 @@ const server = http.createServer((req, res) => {
       .on("data", (chunk) => {
         const objdata = JSON.parse(chunk);
         const arrData = [objdata];
-        // console.log(arrData[0].main.temp);
 
         const realTimeData = arrData
           .map((el) => replaceVal(mainfile, el))
           .join("");
         res.write(realTimeData);
-        // console.log(realTimeData);
       })
       .on("end", function (err) {
         if (err) return console.log("connection closed due to errors", err);
